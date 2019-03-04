6f2d76356b5bb577378f1a6c156cfa86eb777894
Payton Erickson <School@users.noreply.github.com>
2019-03-04 10:38:06 -0800

    added right and left feaure

---
+++ AI.cs
@@ -0,0 +1,7 @@
+namespace BattleShip
+{
+    public class AI
+    {
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -19,10 +19,11 @@ namespace BattleShip
             Set_Board(Left_Board);
             Set_Board(AI_board);
 
-            Display_Board(Right_Board);
+            Display_Board(Left_Board, Right_Board);
 
             Console.WriteLine("Press ANY Key To Quit");
             Console.ReadKey();
+            Console.Clear();
         }
 
         private static void Set_Board(char[,] board)
@@ -36,14 +37,30 @@ namespace BattleShip
             }
         }
 
-        private static void Display_Board(char[,] board)
+        private static void Display_Board(char[,] board, char[,] right_board)
         {
-            Console.WriteLine("  |  01 02 03 04 05 06 07 08 09 10 11 12 13 14 15");
-            Console.WriteLine("--|-----------------------------------------------");
+            
+            string s = "\t  |  01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 \t  |  01 02 03 04 05 06 07 08 09 10 11 12 13 14 15";
+            string s2 = "\t--|-----------------------------------------------\t--|-----------------------------------------------";
+
+            Console.WriteLine(s);
+            Console.WriteLine(s2);
 
             //Lines 1-9 because 2 digit numbers messup the pattern
             for(int x = 0; x < 15; x++)
             {
+                if(x < 9)
+                Console.Write("\t" + (x + 1) + " |  ");
+                else
+                Console.Write("\t" + (x + 1) + "|  ");
+
+                for(int y = 0; y < 15; y++)
+                {
+                    Console.Write(board[x,y] + "  ");
+                }
+
+                Console.Write("\t");
+
                 if(x < 9)
                 Console.Write((x + 1) + " |  ");
                 else
@@ -51,7 +68,7 @@ namespace BattleShip
 
                 for(int y = 0; y < 15; y++)
                 {
-                    Console.Write(board[x,y] + "  ");
+                    Console.Write(right_board[x,y] + "  ");
                 }
 
                 Console.WriteLine();
